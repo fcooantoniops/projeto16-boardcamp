18be75129dd4bec57ef353e65428ffbbbfb2c892
Francisco Antonio Paiva <fcooantoniops@gmail.com>
2022-12-12 21:22:31 -0300

    fix connection string and add.env on .gitignore

--- src/db/database.js
+++ src/db/database.js
@@ -3,11 +3,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connection = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 760497,
-    database: 'boardcamp'
+    connectionString: process.env.DATABASE_URL,
 });
 
 export { connection };
