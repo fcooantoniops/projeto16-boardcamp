3db52e5808a989c16ef401c0a1ebdc10e35ccf98
Francisco Antonio Paiva <fcooantoniops@gmail.com>
2022-12-13 11:03:21 -0300

    route post/categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -11,4 +11,30 @@ async function list(req, res){
     }
 }
 
-export { list };
+async function insert(req, res){
+
+    const { name } = req.body;
+    if(!name) {
+        res.sendStatus(400);
+    }
+
+    const { rows } = await connection.query("SELECT COUNT(1) FROM categories WHERE (name) = $1;",[name]);
+    if (rows[0].count > "0") {
+        return res.sendStatus(409);
+    }
+
+    try {
+        const result = await connection.query("INSERT INTO categories (name) VALUES ($1)", [name]);
+        
+        //console.log(result);
+        if(result.rowCount > 0){
+            return res.sendStatus(201);
+        }
+        return res.sendStatus(404);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export { list, insert };
--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { list } from "../controllers/categoriesController.js"; 
+import { list, insert } from "../controllers/categoriesController.js"; 
 
 const router = express.Router();
 
 router.get('/categories', list);
+router.post('/categories', insert);
 
 export default router;
