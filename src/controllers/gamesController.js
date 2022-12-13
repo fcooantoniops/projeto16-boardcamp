b583d91f6d46a3adf0f83a44f13c386e141c687f
Francisco Antonio Paiva <fcooantoniops@gmail.com>
2022-12-13 15:12:28 -0300

    route post/games

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -25,8 +25,35 @@ async function list(req, res){
     }
 }
 
-async function insert(){
+async function insert(req, res){
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
 
+    if(!name || !image || !stockTotal || !categoryId || !pricePerDay || stockTotal <= 0 || pricePerDay <= 0){
+        return res.sendStatus(404);
+    }
+
+    try {
+        let { rows } = await connection.query("SELECT COUNT(1) FROM games WHERE (name) = $1;",[name]);
+        if (rows[0].count > "0") {
+            return res.sendStatus(409);
+        }
+
+        ({ rows } = await connection.query("SELECT COUNT(1) FROM categories WHERE (id) = $1;",[categoryId]));
+        if (rows[0].count === "0") {
+            return res.sendStatus(400);
+        }
+
+        const result = await connection.query(
+        'INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);',
+        [name, image, stockTotal, categoryId, pricePerDay]);
+        if(result.rowCount > 0){
+            return res.sendStatus(201);
+        }
+        return res.sendStatus(404);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
 
-export { list, insert }
+export { list, insert };
